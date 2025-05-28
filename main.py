8a0f98de96c891d3449ff8152254f5f7a933cd1d
RiordanKemp <123030961+RiordanKemp@users.noreply.github.com>
2025-05-28 21:33:35 +0000

    added python packages

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 import arrr
 from pyscript import document
 import random
+import os
+import os.path
+import re
+import csv
 
 
 def translate_english(event):
@@ -8,4 +12,5 @@ def translate_english(event):
     english = input_text.value
     output_div = document.querySelector("#output")
     output_div.innerText = arrr.translate(english)
-    output_div.innerText = 3
+    rand_int = random.randrange(2, 1001)
+    output_div.innerText = rand_int
