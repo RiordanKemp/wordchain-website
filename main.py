837cc54aed477a85d4a6ae29cc6385c66a6170bf
RiordanKemp <123030961+RiordanKemp@users.noreply.github.com>
2025-05-28 23:25:52 +0000

    visual changes

--- main.py
+++ main.py
@@ -6,6 +6,24 @@ import os.path
 import re
 import csv
 
+openingStr = """Welcome to NAME PENDING (word chain??)!"""
+difficultyDetails = "\nHard Mode requires child words to be +-1 in length relative to their parent."
+hardmodeEnabled = "\nYou've chosen to play on Hard Mode.  Hard Mode requires child words to be +-1 in length relative to their parent."
+exitMessage = "Thanks for playing! Exiting now..."
+reset_message = "Resetting the round now.."
+word_doesnt_exist_error = "This word does not exist."
+
+difficultyOn = False
+
+directory_str = "dict_directory"
+
+alphabet_dict = {}
+words_dict = {}
+numb_dict = {}
+starting_words_set = set()
+already_used_words_set = set()
+valid_letters_dict = {}
+
 
 def translate_english(event):
     input_text = document.querySelector("#english")
@@ -13,4 +31,10 @@ def translate_english(event):
     output_div = document.querySelector("#output")
     output_div.innerText = arrr.translate(english)
     rand_int = random.randrange(2, 1001)
-    output_div.innerText = rand_int
+    output_div.innerText = str(rand_int) + openingStr
+    print(openingStr)
+
+def test_function(event):
+    input_text = document.querySelector("#english")
+    output_div = document.querySelector("#output")
+    output_div.innerText = difficultyDetails
